7df4e84ee26de96ed91c3da40df7ee1205a3672f
jerry-tech <53123555+jerry-tech@users.noreply.github.com>
2020-04-25 15:47:57 -0700

    Initial Commit

---
+++ app/src/main/java/com/accounting/ant/MainActivity.java
@@ -0,0 +1,31 @@
+package com.accounting.ant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    TextView footerTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        footerTime = findViewById(R.id.footerTime);
+
+        Calendar calendar = Calendar.getInstance();
+        footerTime.setText(calendar.getTime().toString());
+    }
+    public void nextRegister(View view){
+        Intent intent = new Intent(this,registration.class);
+        startActivity(intent);
+
+        overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
+    }
+}
