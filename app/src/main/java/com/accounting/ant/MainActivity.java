dad27931b56a02104b0d09d87ba5db5988e2cf49
ObsidianBlaze <wijojames@gmail.com>
2020-04-28 07:12:06 +0100

    Hooking the login page with the database off the API

--- app/src/main/java/com/accounting/ant/MainActivity.java
+++ app/src/main/java/com/accounting/ant/MainActivity.java
@@ -2,30 +2,128 @@ package com.accounting.ant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.toolbox.StringRequest;
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    TextView footerTime;
+	//Creating the tag to handle success or cancel events.
+	private static final String TAG = "LoginActivity";
+	//The url for the LoginAPI
+	private static final String URL_FOR_LOGIN = "https://antquakes.codeweb.com.ng/API/login.php";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	ProgressDialog progressDialog;
+	TextInputEditText username, password;
+	Button loginBtn;
 
-        footerTime = findViewById(R.id.footerTime);
+	TextView footerTime;
 
-        Calendar calendar = Calendar.getInstance();
-        footerTime.setText(calendar.getTime().toString());
-    }
-    public void nextRegister(View view){
-        Intent intent = new Intent(this,registration.class);
-        startActivity(intent);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
-    }
+		// Progress dialog
+		progressDialog = new ProgressDialog(this);
+		progressDialog.setCancelable(false);
+		//Getting the username, password and login button.
+		username = findViewById(R.id.emailAdd);
+		password = findViewById(R.id.txtPass);
+		loginBtn = findViewById(R.id.loginBtn);
+
+
+		//Setting the footer of the application to the current date.
+		footerTime = findViewById(R.id.footerTime);
+
+		Calendar calendar = Calendar.getInstance();
+		footerTime.setText(calendar.getTime().toString());
+
+		//Button to log in user.
+		loginBtn.setOnClickListener(event -> {
+			//Implementing the login method
+			loginUser(username.getText().toString(),password.getText().toString());
+		});
+	}
+
+	//Logic for login button.
+	private void loginUser(final String username, final String password) {
+		// Tag used to cancel the request
+		String cancel_req_tag = "login";
+		progressDialog.setMessage("Logging you in...");
+		showDialog();
+		StringRequest strReq = new StringRequest(Request.Method.POST,
+				URL_FOR_LOGIN, response -> {
+			Log.d(TAG, "Register Response: " + response);
+			hideDialog();
+			try {
+				JSONObject jObj = new JSONObject(response);
+				boolean error = jObj.getBoolean("error");
+
+				//Logic for correct login information
+				if (!error) {
+//					String user = jObj.getJSONObject("user").getString("name");
+					Toast.makeText(MainActivity.this, "Working perfectly", Toast.LENGTH_SHORT).show();
+				} else {
+
+					String errorMsg = jObj.getString("text");
+					Toast.makeText(getApplicationContext(),
+							errorMsg, Toast.LENGTH_LONG).show();
+				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+
+		}, error -> {
+			Log.e(TAG, "Login Error: " + error.getMessage());
+			Toast.makeText(getApplicationContext(),
+					error.getMessage(), Toast.LENGTH_LONG).show();
+			hideDialog();
+		}) {
+			//Mapping the users input with the database user information
+			@Override
+			protected Map<String, String> getParams() {
+				// Posting params to login url
+				Map<String, String> params = new HashMap<>();
+				params.put("username", username);
+				params.put("password", password);
+				return params;
+			}
+		};
+		// Adding request to request queue
+		AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(strReq, cancel_req_tag);
+	}
+
+	private void showDialog() {
+		if (!progressDialog.isShowing())
+			progressDialog.show();
+	}
+
+	private void hideDialog() {
+		if (progressDialog.isShowing())
+			progressDialog.dismiss();
+	}
+
+	//Moving to the registeration page.
+	public void nextRegister(View view) {
+		Intent intent = new Intent(this, registration.class);
+		startActivity(intent);
+
+		overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+	}
 }
