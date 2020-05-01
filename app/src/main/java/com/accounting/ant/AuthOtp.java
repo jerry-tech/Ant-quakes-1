fa0d20511e048de657050a16cc4f2845a5d4aeed
jerry-tech <53123555+jerry-tech@users.noreply.github.com>
2020-05-01 13:49:25 -0700

    Addition of Referral Id

--- app/src/main/java/com/accounting/ant/AuthOtp.java
+++ app/src/main/java/com/accounting/ant/AuthOtp.java
@@ -1,14 +1,14 @@
 package com.accounting.ant;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
 
 public class AuthOtp extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_auth_otp);
+
     }
 }
